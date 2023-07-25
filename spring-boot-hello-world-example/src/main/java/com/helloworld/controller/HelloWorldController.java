10bd1a4304ed142dc5a07a521b3a03d36c087ccf
Pranay Sahare <psahare@delaplex.com>
2023-07-25 15:37:42 +0530

    Merge this into main branch

--- spring-boot-hello-world-example/src/main/java/com/helloworld/controller/HelloWorldController.java
+++ spring-boot-hello-world-example/src/main/java/com/helloworld/controller/HelloWorldController.java
@@ -7,6 +7,6 @@ public class HelloWorldController
 @RequestMapping("/")
 public String hello() 
 {
-return "<h1> Congratulations. You have successfully deployed the sample Spring Boot Application with GitHub Actions. </h1>";
+return "<h1> Congratulations. You have successfully deployed the sample Spring Boot Application with GitHub Actions from develop-1 branch. </h1>";
 }
 }
